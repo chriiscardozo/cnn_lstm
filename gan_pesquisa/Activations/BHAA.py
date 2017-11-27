d4d3ea70223498d9a1c6844843e8a6b4da2afeb3
Christian Cardozo <chriiscardozo@gmail.com>
2017-11-27 16:18:35 -0200

    bug fixes and outputs and custom activations

---
+++ gan_pesquisa/Activations/BHAA.py
@@ -0,0 +1,67 @@
+from keras import backend as K
+from keras.engine.topology import Layer
+import numpy as np
+from keras import initializers
+from keras.engine import InputSpec
+
+class BHAA(Layer):
+
+	def __init__(self,
+		lambda_initializer=initializers.Constant(value=1),
+		t1_initializer=initializers.glorot_normal(),
+		t2_initializer=initializers.glorot_normal(),
+		dominio_0_1=True,
+		shared_axes=None, **kwargs):
+		super(BHAA, self).__init__(**kwargs)
+
+		self.lambda_initializer = initializers.get(lambda_initializer)
+		self.t1_initializer = initializers.get(t1_initializer)
+		self.t2_initializer = initializers.get(t2_initializer)
+		self.dominio_0_1 = dominio_0_1
+
+		if(shared_axes == None):
+			self.shared_axes = None
+		elif not isinstance(shared_axes, (list, tuple)):
+			self.shared_axes = [shared_axes]
+		else:
+			self.shared_axes = list(shared_axes)
+
+	def build(self, input_shape):
+		param_shape = list(input_shape[1:])
+		self.param_broadcast = [False] * len(param_shape)
+		if self.shared_axes is not None:
+			for i in self.shared_axes:
+				param_shape[i - 1] = 1
+				self.param_broadcast[i - 1] = True
+
+		# lambda
+		self.l = self.add_weight(name='l', shape=param_shape, initializer=self.lambda_initializer)
+		# tau 1
+		self.t1 = self.add_weight(name='t1', shape=param_shape, initializer=self.t1_initializer)
+		# tau 2
+		self.t2 = self.add_weight(name='t2', shape=param_shape, initializer=self.t2_initializer)
+
+		axes = {}
+		if(self.shared_axes):
+			for i in range(1, len(input_shape)):
+				if(i not in self.shared_axes):
+					axes[i] = input_shape[i]
+
+		self.input_spec = InputSpec(ndim=len(input_shape), axes=axes)
+		self.built = True
+
+	def call(self, inputs):
+		# bi-hiperbolic assimetric adaptative
+		# bhaa(l, t1, t2) = h1 - h2
+		# h1 = sqrt(( (l^2)*((x + (1/(2*l)))^2) ) + t1^2)
+		# h2 = sqrt(( (l^2)*((x + (1/(2*l)))^2) ) + t2^2)
+		pot = 1
+		soma = 0
+		if(self.dominio_0_1):
+			pot = 2
+			soma = 0.5
+
+		h1 = K.sqrt( (K.square((K.pow(2.0, pot)*self.l*inputs) + 1.0)/K.pow(4.0,pot)) + K.square(self.t1) )
+		h2 = K.sqrt( (K.square((K.pow(2.0, pot)*self.l*inputs) - 1.0)/K.pow(4.0,pot)) + K.square(self.t2) )
+		
+		return (h1 - h2) + soma
---
+++ gan_pesquisa/Activations/SHReLU.py
@@ -0,0 +1,42 @@
+from keras import backend as K
+from keras.engine.topology import Layer
+import numpy as np
+from keras import initializers
+from keras.engine import InputSpec
+
+class SHReLU(Layer):
+
+	def __init__(self, alpha_initializer=initializers.Constant(value=0.1), shared_axes=None, **kwargs):
+		super(SHReLU, self).__init__(**kwargs)
+
+		self.alpha_initializer = initializers.get(alpha_initializer)
+
+		if(shared_axes == None):
+			self.shared_axes = None
+		elif not isinstance(shared_axes, (list, tuple)):
+			self.shared_axes = [shared_axes]
+		else:
+			self.shared_axes = list(shared_axes)
+
+	def build(self, input_shape):
+		param_shape = list(input_shape[1:])
+		self.param_broadcast = [False] * len(param_shape)
+		if self.shared_axes is not None:
+			for i in self.shared_axes:
+				param_shape[i - 1] = 1
+				self.param_broadcast[i - 1] = True
+
+		self.alpha = self.add_weight(name='alpha', shape=param_shape, initializer=self.alpha_initializer)
+
+		axes = {}
+		if(self.shared_axes):
+			for i in range(1, len(input_shape)):
+				if(i not in self.shared_axes):
+					axes[i] = input_shape[i]
+
+		self.input_spec = InputSpec(ndim=len(input_shape), axes=axes)
+		self.built = True
+
+	def call(self, inputs):
+		# shrelu(x,alpha) = (x + sqrt(x^2 + alpha^2)/2)
+		return (inputs + K.sqrt(K.square(inputs) + K.square(self.alpha)))/2.0
